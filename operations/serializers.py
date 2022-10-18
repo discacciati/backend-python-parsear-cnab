6ad34c36ede663f9711ac626fb95bf580a050a7d
Eliane Discacciati <discacciati@gmail.com>
2022-10-18 02:47:09 -0300

    Fix: form

--- operations/serializers.py
+++ operations/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
+from stack_data import Serializer
 
 from operations.models import Operations
+from .file import FileCnab
 
 class OperationsSerializer(serializers.ModelSerializer):
     class Meta: 
         model= Operations
         fields= "__all__"
+    
+  
+    
+    
