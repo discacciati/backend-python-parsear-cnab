3dca3b44d60d8604996e0da5fd43321e331f061d
Eliane Discacciati <discacciati@gmail.com>
2022-10-18 02:51:59 -0300

    Fix: serializer

--- operations/serializers.py
+++ operations/serializers.py
@@ -2,20 +2,12 @@ from rest_framework import serializers
 from stack_data import Serializer
 
 from operations.models import Operations
-from .file import FileCnab
+
 
 class OperationsSerializer(serializers.ModelSerializer):
     class Meta: 
         model= Operations
         fields= "__all__"
     
-    def create(self, validated_data): 
-        full_file = FileCnab(validated_data.get("file"))
-        
-        for item in full_file.operations: 
-            serializer = OperationsSerializer(data=item)
-            serializers.is_valid(raise_exception=True)
-            serializer.save()
-        
-    
+
     
